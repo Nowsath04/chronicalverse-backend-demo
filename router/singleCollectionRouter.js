c01b5f000cf51964aa64d48aa3600f174a2b768f
pratheeshraj <rajeshnsk5@gmail.com>
2023-12-05 16:57:17 +0530

    added

--- router/singleCollectionRouter.js
+++ router/singleCollectionRouter.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const jwtVerification = require('../middleware/jwtVerification');
 const upload = require('../utils/fileUpload');
-const { SingleCollectionController, nftDataController, nftAllData, nftPathId, getUserNFT, updateUserNFt } = require('../controller/SingleCollectionController');
+const { SingleCollectionController, nftDataController, nftAllData, nftPathId, getUserNFT, updateUserNFt, UpdateNft } = require('../controller/SingleCollectionController');
 
 
 
 const router = express.Router()
 
 router.post("/new-single-collection",jwtVerification,upload.single("image"),nftDataController)
-router.get("/nft-all-data",nftAllData)
-router.get("/getnft/:id/:token/:collectionId",getUserNFT)
+router.post("/update-nft/:tokenid/:collectionId",UpdateNft)
+
+// router.get("/nft-all-data",nftAllData)
+// router.get("/getnft/:id/:token/:collectionId",getUserNFT)
 
 module.exports = router
